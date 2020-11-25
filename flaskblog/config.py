f81f726d6b3c91894b7d245a4da257c473a4dafd
Your Name <rishabhdhyani420@gmail.com>
2020-11-25 16:21:51 +0530

    Added my project

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '6755b79ff83a602ad6990894b5761cb0'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'rishabdhyani4@gmail.com'
+    MAIL_PASSWORD = 'Rishabh@1234'
